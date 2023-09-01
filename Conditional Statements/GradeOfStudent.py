d86a210b2de45f9c96c0161389f9d2d4067df525
Harshal Amrutkar <64520319+harshtheking100@users.noreply.github.com>
2023-09-01 08:53:58 +0530

    Add files via upload

--- Conditional Statements/GradeOfStudent.py
+++ Conditional Statements/GradeOfStudent.py
@@ -1,18 +1,12 @@
-m1 = int(input('Enter marks for sub1 : ')) 
-m2 = int(input('Enter marks for sub2 : ')) 
-m3 = int(input('Enter marks for sub3 : ')) 
-m4 = int(input('Enter marks for sub4 : ')) 
-m5 = int(input('Enter marks for sub5 : ')) 
+percent = int(input('Enter Percent for Student: '))
 
-percent = (m1+m2+m3+m4+m5)/5
+print()
 
-if percent >= 75 :
-    print('Distinction')
-elif 60 <= percent < 75 :
-    print('First Class')
-elif 45 <= percent < 60 :
-    print('Second Class')
-elif 35 <= percent < 45 :
-    print('Third Class')
-else :
-    print('Fail')
+if percent > 90 :
+    print('Grade is A')
+elif 80 < percent <= 90 :
+    print('Grade is B')
+elif 60 <= percent <= 80 :
+    print('Grade is C')
+elif percent < 60 :
+    print('Grade is D')
